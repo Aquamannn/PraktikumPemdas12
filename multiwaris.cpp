4beb306b1e465e6ae2419ef7fa9e71cde7d18048
Lenovo <9.3razzinayyuri@gmail.com>
2025-06-03 18:22:59 +0700

    membuat class orang pada file multi waris

---
+++ multiwaris.cpp
@@ -0,0 +1,11 @@
+#include <isotream>
+using namespace std;
+
+class orang{
+    public:
+    int umur;
+    orang(int pUmur) :
+    umur(pUmur) {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
