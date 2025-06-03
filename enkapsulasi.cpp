c6d8a5eeba14ac7f214f17392232dd2a3d0f4a02
Lenovo <9.3razzinayyuri@gmail.com>
2025-06-03 18:17:10 +0700

    memanggil data class remote lampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,18 @@ class remoteLampu {
         return saklarNo[i];
     }
 };
+
+int main(){
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklaNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklaNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklaNo(2, "Lampu Ruang Tidur");
+    lampuRumah.setSaklaNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklaNo(0) << endl;
+    cout << lampuRumah.getSaklaNo(1) << endl;
+    cout << lampuRumah.getSaklaNo(2) << endl;
+    cout << lampuRumah.getSaklaNo(3) << endl;
+
+    return 0;
+}
